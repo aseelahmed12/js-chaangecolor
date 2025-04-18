d505a7364a15448942d77ac8049fbbe271488bde
aseelahmed12 <aseelahmed9692@gmail.com>
2025-04-18 19:26:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// document.getElementById("bth").onclick= changeheading;
+// function changeheading(){
+//     document.getElementById("hadding").textContent="welcome";
+//     document.getElementById("hadding").style.backgroundColor = "red";
+
+// }
+
+    
+document.getElementById("bth").onclick = function () {
+    document.getElementById("hadding").textContent = "Welcome!";
+    document.body.style.backgroundColor = "lightblue";
+};
